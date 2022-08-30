13e62efef21382ca612d39e7f68956876e03c829
JIwonKwon <rnjswldnjs1012@gmail.com>
2022-08-30 12:11:04 +0900

    ✨commit

---
+++ NC2/NC2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NC2
+//
+//  Created by kwon ji won on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
